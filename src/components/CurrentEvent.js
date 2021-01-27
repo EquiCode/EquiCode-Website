23047daf2ba441dd5e118510c30fd24a6d6dcacc
srivastavayushi <srivastavayushi@users.noreply.github.com>
2021-01-27 20:21:58 +0530

    routing

---
+++ src/components/CurrentEvent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CurrentEvent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Current Event</h1>
+      </div>
+    );
+  }
+}
+
+export default CurrentEvent;
---
+++ src/components/Events.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Events extends Component {
+  render() {
+    return (
+      <div>
+        <h1>All Events</h1>
+      </div>
+    );
+  }
+}
+
+export default Events;

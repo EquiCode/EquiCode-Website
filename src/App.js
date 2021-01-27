48b04c1710bcc4132361243f47966150b6e3d975
kaustuvkaran01 <kaustuvkaran01@gmail.com>
2021-01-27 20:37:04 +0530

    Routing init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./components/Home";
 import "./App.css";
-
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 function App() {
   return (
+    <Router>
     <div className="App">
       <Home />
       <div>EquiCode</div>
     </div>
+    </Router>
   );
 }
 

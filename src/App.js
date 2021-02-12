1bf6cd1b780aa9b504b9736c3a5e7e14f04c6656
kaustuvkaran01 <kaustuvkaran01@gmail.com>
2021-02-12 14:40:31 +0530

    Appjs debugged

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 // import Home from "./components/Home";
-import AboutUs from "./components/Aboutus";
+// import AboutUs from "./components/Aboutus";
 // import Contactus from "./components/Contactus";
 
 import Events from "./components/Events";
 import Footer from "./components/Footer";
 import Volunteer from "./pages/Volunteer";
 import Team from "./pages/Team";
-import Testimonials from "./components/Testimonials";
+// import Testimonials from "./components/Testimonials";
 // import Sponsors from "./components/Sponsors";
 
 import NavbarNew from "./components/NavbarNew";

fd4b593f22a5593d83693508f89cba4f85f56d1a
srivastavayushi <srivastavayushi@users.noreply.github.com>
2021-03-23 00:01:47 +0530

    made axios-post request

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,78 +1,114 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
-export default function Sponsors() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert("Thank you for submitting the form ! Our team will contact you soon");
+class Sponsors extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+    };
+  }
+
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   };
-  return (
-    <StyledSponsor id="sponsors">
-      <section class="text-gray-600 body-font relative">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="flex flex-col text-center w-full mb-12">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-              Contact Us
-            </h1>
-            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
-              For any queries, Contact us here:
-            </p>
-          </div>
-          <div class="lg:w-1/2 md:w-2/3 mx-auto">
-            <div class="flex flex-wrap -m-2">
-              <div class="p-2 w-1/2">
-                <div class="relative">
-                  <label for="name" class="leading-7 text-sm text-gray-600">
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    axios
+      .post(
+        "https://sheet.best/api/sheets/42f8b18f-dab8-4763-a395-ad95093d035f",
+        this.state
+      )
+      .then((response) => {
+        console.log(response);
+      });
+  };
+  render() {
+    const { name, email, message } = this.state;
+    return (
+      <StyledSponsor id="sponsors">
+        <section class="text-gray-600 body-font relative">
+          <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-col text-center w-full mb-12">
+              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+                Contact Us
+              </h1>
+              <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+                For any queries, Contact us here:
+              </p>
+            </div>
+            <div class="lg:w-1/2 md:w-2/3 mx-auto">
+              <div class="flex flex-wrap -m-2">
+                <div class="p-2 w-1/2">
+                  <div class="relative">
+                    <label class="leading-7 text-sm text-gray-600">Name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      value={name}
+                      onChange={this.changeHandler}
+                      placeholder="Enter your Name"
+                      class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div class="p-2 w-1/2">
-                <div class="relative">
-                  <label for="email" class="leading-7 text-sm text-gray-600">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
+                <div class="p-2 w-1/2">
+                  <div class="relative">
+                    <label for="email" class="leading-7 text-sm text-gray-600">
+                      Email
+                    </label>
+                    <input
+                      type="text"
+                      name="email"
+                      value={email}
+                      onChange={this.changeHandler}
+                      placeholder="Enter your Name"
+                      class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div class="p-2 w-full">
-                <div class="relative">
-                  <label for="message" class="leading-7 text-sm text-gray-600">
-                    Message
-                  </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                  ></textarea>
+                <div class="p-2 w-full">
+                  <div class="relative">
+                    <label
+                      for="message"
+                      class="leading-7 text-sm text-gray-600"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      type="text"
+                      name="message"
+                      value={message}
+                      onChange={this.changeHandler}
+                      placeholder="Enter your Name"
+                      class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    ></textarea>
+                  </div>
                 </div>
-              </div>
-              <div class="p-2 w-full">
-                <button
-                  class="flex mx-auto text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg"
-                  onClick={handleSubmit}
-                >
-                  Button
-                </button>
+                <div class="p-2 w-full">
+                  <button
+                    type="submit"
+                    class="flex mx-auto text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg"
+                  >
+                    Button
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </StyledSponsor>
-  );
+        </section>
+      </StyledSponsor>
+    );
+  }
 }
+
+export default Sponsors;
 
 const StyledSponsor = styled.div`
   .style-form {
--- src/components/events/NextEvent.js
+++ src/components/events/NextEvent.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 import image from "../../vector/question.svg";
+import axios from "axios";
 
-export default function NextEvent() {
-  return (
-    <>
+class NextEvent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      idea: "",
+    };
+  }
+
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    axios
+      .post(
+        "https://sheet.best/api/sheets/a759e803-db84-4861-a545-6ed3a4ec1db3",
+        this.state
+      )
+      .then((response) => {
+        console.log(response);
+      });
+  };
+  render() {
+    const { name, email, idea } = this.state;
+    return (
       <StyledEvent>
         <section class="text-gray-600 body-font">
           <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
@@ -21,7 +49,7 @@ export default function NextEvent() {
               <h2 class="text-gray-900 text-lg font-medium title-font mb-3">
                 Sign Up
               </h2>
-              <form autocomplete="off">
+              <form autocomplete="off" onSubmit={this.submitHandler}>
                 <div class="relative mb-4">
                   <label
                     for="full-name"
@@ -30,10 +58,11 @@ export default function NextEvent() {
                     Full Name
                   </label>
                   <input
-                    placeholder="Enter your Name"
                     type="text"
-                    id="full-name"
-                    name="full-name"
+                    name="name"
+                    value={name}
+                    onChange={this.changeHandler}
+                    placeholder="Enter your Name"
                     class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
@@ -44,7 +73,9 @@ export default function NextEvent() {
                   <input
                     placeholder="Enter your email"
                     type="email"
-                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={this.changeHandler}
                     class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
@@ -56,10 +87,12 @@ export default function NextEvent() {
                     Your preferences
                   </label>
                   <textarea
-                    rows="4"
-                    cols="50"
-                    name="comment"
-                    form="usrform"
+                    cols="20"
+                    rows="3"
+                    type="text"
+                    value={idea}
+                    onChange={this.changeHandler}
+                    name="idea"
                     class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                     placeholder="Enter text here"
                   ></textarea>
@@ -76,9 +109,11 @@ export default function NextEvent() {
           </div>
         </section>
       </StyledEvent>
-    </>
-  );
+    );
+  }
 }
+
+export default NextEvent;
 
 const StyledEvent = styled.div`
   background: url(${image});

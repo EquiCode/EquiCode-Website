d5dacd476d4a6994651831a7aa324fa4d76a8953
srivastavayushi <srivastavayushi@users.noreply.github.com>
2021-04-25 22:59:08 +0530

    edited screen

--- tailwind.js
+++ tailwind.js
@@ -9,12 +9,8 @@ module.exports = {
       sm: "640px",
       md: "768px",
       lg: "1024px",
-      xl: "1280px",
+      xl: "1240px",
       // "2xl": "1536px",
-      // sm: "100%",
-      // md: "100%",
-      // lg: "1024px",
-      // xl: "1280px",
     },
     colors: {
       transparent: "transparent",

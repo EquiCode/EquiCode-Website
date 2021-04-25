56c0c16fd504bfc0046c63909c91d8032ed788ee
srivastavayushi <srivastavayushi@users.noreply.github.com>
2021-04-25 22:30:12 +0530

    edited screen dimensions

--- tailwind.js
+++ tailwind.js
@@ -6,15 +6,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      // sm: '640px',
-      // md: '768px',
-      // lg: '1024px',
-      // xl: '1280px',
-      // '2xl': '1536px',
-      sm: "100%",
-      md: "100%",
+      sm: "640px",
+      md: "768px",
       lg: "1024px",
       xl: "1280px",
+      // "2xl": "1536px",
+      // sm: "100%",
+      // md: "100%",
+      // lg: "1024px",
+      // xl: "1280px",
     },
     colors: {
       transparent: "transparent",
